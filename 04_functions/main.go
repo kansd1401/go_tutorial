aa327f45c4391175a8553bb15c994ae183ed47b4
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-01-28 15:53:13 -0800

    learnt basic functions

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -6,6 +6,11 @@ func greeting(name string) string {
 	return "Hellow " + name
 }
 
+func getSum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
 	fmt.Println(greeting("Seymor"))
+	fmt.Println(getSum(1, 2))
 }
