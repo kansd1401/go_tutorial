a85ddf9ec45f3caccccc302ad9c94d3f3afa001e
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-01-28 17:32:52 -0800

    arrays done

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var fruitArr [4]string
-	fruitArr[0] = "Apple"
-	fruitArr[1] = "Orange"
-	fruitArr[2] = "Banana"
-	fruitArr[3] = "Peach"
-
-	fmt.Println(fruitArr)
+	// var fruitArr [4]string
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+	// fruitArr[2] = "Banana"
+	// fruitArr[3] = "Peach"
+	fruitSlice := []string{"Apple", "Orange", "Banana"}
+	fmt.Println(len(fruitSlice))
 }
