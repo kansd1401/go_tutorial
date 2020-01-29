bed1df0d174f42818d8ca617e310c6d1c0341a21
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-01-28 17:30:39 -0800

    arrays done

---
+++ 05_arrays_slices/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fruitArr [4]string
+	fruitArr[0] = "Apple"
+	fruitArr[1] = "Orange"
+	fruitArr[2] = "Banana"
+	fruitArr[3] = "Peach"
+
+	fmt.Println(fruitArr)
+}
